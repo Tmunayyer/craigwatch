60633cd5662906155a69b10d0c4b514cbd05cf8f
tmunayyer <tmunayyer@gmail.com>
2020-06-17 22:03:15 -0400

    refactor monitor url and related tests

--- api_test.go
+++ api_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -11,6 +13,31 @@ import (
 	"github.com/stretchr/testify/assert"
 	craigslist "github.com/tmunayyer/go-craigslist"
 )
+
+const (
+	badCraigslistURL = "www.badurl.com"
+)
+
+var fakeListings = []craigslist.Listing{
+	{
+		DataPID:      "1",
+		DataRepostOf: "",
+		Date:         "06/19/2020",
+		Title:        "Bananas, organic, not eaten",
+		Link:         "www.craigslist.com/bananapost",
+		Price:        "$100",
+		Hood:         "newyork",
+	},
+	{
+		DataPID:      "2",
+		DataRepostOf: "",
+		Date:         "06/15/2020",
+		Title:        "The best crackers ever",
+		Link:         "www.craigslist.com/crackers",
+		Price:        "$20",
+		Hood:         "newyork",
+	},
+}
 
 type mockCraigslistClient struct {
 	location string
@@ -21,51 +48,91 @@ func (m *mockCraigslistClient) FormatURL(term string, o craigslist.Options) stri
 }
 
 func (m *mockCraigslistClient) GetListings(ctx context.Context, url string) ([]craigslist.Listing, error) {
-	listings := []craigslist.Listing{
-		{
-			DataPID: "1",
-		},
-		{
-			DataPID: "2",
-		},
-		{
-			DataPID: "3",
-		},
+	if url == badCraigslistURL {
+		return nil, fmt.Errorf("invalid url: %v", url)
 	}
 
-	return listings, nil
+	return fakeListings, nil
 }
 
 func TestMonitorURL(t *testing.T) {
-	cl := mockCraigslistClient{
+	mockCL := mockCraigslistClient{
 		location: "newyork",
 	}
 
-	mockHandlerEnv := handlerEnv{
-		cl: &cl,
-	}
+	api := newAPIService(&mockCL)
 
-	t.Run("post - should return data", func(t *testing.T) {
-		// create a new request
-		request, err := http.NewRequest(http.MethodPost, "/api/monitorurl", nil)
+	t.Run("post - no body passed", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodPost, "/", http.NoBody)
 		assert.NoError(t, err)
-		// recorder to pass to spy on response
-		response := httptest.NewRecorder()
+		res := httptest.NewRecorder()
 
-		handleMonitorURL(response, request, &mockHandlerEnv)
+		api.handleMonitorURL(res, req)
 
-		data, err := ioutil.ReadAll(response.Body)
-		assert.NoError(t, err)
+		message, err := ioutil.ReadAll(res.Body)
 
-		expected := []string{"1", "2", "3"}
-		var listings []struct {
-			DataPID string
+		assert.Equal(t, http.StatusBadRequest, res.Code)
+		assert.Equal(t, "could not decode user payload: EOF\n", string(message))
+	})
+
+	t.Run("post - invalid url", func(t *testing.T) {
+		// make a body
+		type body struct {
+			URL string
 		}
-		err = json.Unmarshal(data, &listings)
+
+		b := body{URL: badCraigslistURL}
+		data, err := json.Marshal(b)
+		assert.NoError(t, err)
+		reader := bytes.NewReader(data)
+
+		req, err := http.NewRequest(http.MethodPost, "/", reader)
+		assert.NoError(t, err)
+		res := httptest.NewRecorder()
+
+		api.handleMonitorURL(res, req)
+
+		message, err := ioutil.ReadAll(res.Body)
+
+		assert.Equal(t, http.StatusBadRequest, res.Code)
+		assert.Equal(t, "url provided is not a compatible with craigslist\n", string(message))
+	})
+
+	t.Run("post - recieves data", func(t *testing.T) {
+		// make a body
+		type body struct {
+			URL string
+		}
+
+		b := body{URL: "www.anything.com"}
+		data, err := json.Marshal(b)
+		assert.NoError(t, err)
+		reader := bytes.NewReader(data)
+
+		req, err := http.NewRequest(http.MethodPost, "/", reader)
+		assert.NoError(t, err)
+		res := httptest.NewRecorder()
+
+		// call handelr
+		api.handleMonitorURL(res, req)
+
+		assert.Equal(t, http.StatusOK, res.Code)
+
+		// just check for the pids
+		responseData := struct {
+			Searched string
+			Results  []craigslist.Listing
+		}{}
+
+		// put body into a slice
+		responseBytes, err := ioutil.ReadAll(res.Body)
+		assert.NoError(t, err)
+		err = json.Unmarshal(responseBytes, &responseData)
 		assert.NoError(t, err)
 
-		for i, listing := range listings {
-			assert.Equal(t, expected[i], listing.DataPID)
+		for i, listing := range responseData.Results {
+			assert.Equal(t, fakeListings[i].DataPID, listing.DataPID)
 		}
+
 	})
 }
