96a3a5768cd27227e4f6c1946254e88bba360a35
tmunayyer <tmunayyer@gmail.com>
2020-06-19 11:06:41 -0400

    implemented logger for api errors

--- api.go
+++ api.go
@@ -22,6 +22,14 @@ func newAPIService(cl craigslist.Client, db connection) *apiService {
 	return &api
 }
 
+func apiErrorHandler(w http.ResponseWriter, status int, endpoint string, message string, err error) {
+	fmt.Println("error from handler: " + endpoint)
+	fmt.Println("-- outbound message: " + message)
+	fmt.Println(fmt.Sprintf("-- the error: %v", err))
+
+	http.Error(w, message, http.StatusBadRequest)
+}
+
 func (s *apiService) handleMonitorURL(w http.ResponseWriter, req *http.Request) {
 	type requestBody struct {
 		Email string
@@ -32,14 +40,14 @@ func (s *apiService) handleMonitorURL(w http.ResponseWriter, req *http.Request) 
 	body := requestBody{}
 	err := d.Decode(&body)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("could not decode user payload: %v", err), http.StatusBadRequest)
+		apiErrorHandler(w, http.StatusBadRequest, "handleMonitorURL", "could not decode user payload", err)
 		return
 	}
 
 	// validate the url before we put it in the DB
 	_, err = s.cl.GetListings(req.Context(), body.URL)
 	if err != nil {
-		http.Error(w, fmt.Sprint("url provided is not a compatible with craigslist"), http.StatusBadRequest)
+		apiErrorHandler(w, http.StatusBadRequest, "handleMonitorURL", "url provided is not a compatible with craigslist", err)
 		return
 	}
 
@@ -48,12 +56,13 @@ func (s *apiService) handleMonitorURL(w http.ResponseWriter, req *http.Request) 
 		URL:   body.URL,
 	})
 	if err != nil {
-		http.Error(w, fmt.Sprintf("could not save the information: %v", err), http.StatusInternalServerError)
+		apiErrorHandler(w, http.StatusInternalServerError, "handleMonitorURL", "could not save the information", err)
+		return
 	}
 
 	data, err := json.Marshal(record)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("problems formatting the data: %v", err), http.StatusInternalServerError)
+		apiErrorHandler(w, http.StatusInternalServerError, "handleMonitorURL", "problems formatting the data", err)
 		return
 	}
 
