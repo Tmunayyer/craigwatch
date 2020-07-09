08df67683e7ccb073ed1fe9dcad60b93a65f2fa2
tmunayyer <tmunayyer@gmail.com>
2020-07-09 14:41:28 -0400

    rendering search results on result page

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -1,18 +1,15 @@
 <template>
-  <div id='listingItem'>
-  
-  <h2 style="text-decoration: bold" >{{ listing.name }}</h2>
-  <h4>posted on: {{ listing.date }}</h4>
-  <p >{{ listing.content }}</p>
-  <h4>posting id: {{ listing.id }} </h4>
- 
+  <div id="listingItem">
+    <h2 style="text-decoration: bold">{{ listing.Title }}</h2>
+    <h4>posted on: {{ listing.UnixDate }}</h4>
+    <p>{{ listing.Link }}</p>
+    <h4>price: ${{ listing.Price }}</h4>
   </div>
 </template>
 
 <script>
-  
-  export default {
-    name: 'Listing',
-    props: ['listing', 'index']
-  }
+export default {
+  name: "Listing",
+  props: ["listing", "index"]
+};
 </script>
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,48 +1,63 @@
 <template>
-<div> 
-  <ul>Results:
-  <div id="resultsBox">
-    <div v-if="newListings.length"> {{ newListings.length }} new listings found</div>
-    <button v-on:click="()=>{listings}" v-if="newListings.length">Load</button>
-    <div v-for="(listing, index) in listings" v-bind:key="listing.url">
-    <Listing v-bind:listing="listing" v-bind:index="index" ></Listing>
-    </div>
-  </div>
-  </ul>
+  <div>
+    <ul>
+      <div>Search Results For:</div>
+      <div>Name:</div>
+      <div>URl:</div>
+
+      <div id="resultsBox">
+        <div v-for="(listing, index) in resultList" v-bind:key="listing.URL">
+          <Listing v-bind:listing="listing" v-bind:index="index"></Listing>
+        </div>
+      </div>
+    </ul>
   </div>
 </template>
 
 <script>
- import Listing from './Listing.vue';
+import Listing from "./Listing.vue";
 
-  export default {
-    name: 'Results',
-    data() {
-      return {
-      listings: [
-        {name: 'post1', content: 'posting content1', date: '12/19/19', id: '1000293'}
-        ],
-        newListings: [],
-        searchId: 1
-        }
+export default {
+  name: "Results",
+  components: {
+    Listing
+  },
+  data() {
+    return {
+      resultList: [],
+      newListings: [],
+      searchID: this.$route.params.ID,
+      // UnixTimestamp is the cutoff to use when requesting only new listings, should be 0 on load
+      unixTimestamp: 0
+    };
+  },
+  beforeMount: async function() {
+    const resultList = await this.getResultList();
+
+    this.resultList = resultList.Listings;
+  },
+  methods: {
+    getResultList: async function getResultList() {
+      const response = await fetch(
+        `/api/v1/listing?ID=${this.searchID}&Datetime=${this.unixTimestamp}`
+      );
+      const list = await response.json();
+
+      return list;
     },
-    methods: {
-      update: () => {
-          async function getSearchList() {
-            const response = await fetch(`/api/v1/listing?ID=${this.searchId}&Datetime=${Date.now()}`)
-            return await response.json()
-            //post new listings or j the price somewhere too?
-        }
-        var results = getSearchList()
-        if(results.HasNewListings){
-           this.listings = this.newListings.concat(results.Listings)
-        }
-        
+    update: () => {
+      async function getSearchList() {
+        const response = await fetch(
+          `/api/v1/listing?ID=${this.searchId}&Datetime=${Date.now()}`
+        );
+        return await response.json();
+        //post new listings or j the price somewhere too?
       }
-    },
-    components: {
-      Listing
+      var results = getSearchList();
+      if (results.HasNewListings) {
+        this.listings = this.newListings.concat(results.Listings);
+      }
     }
-
   }
+};
 </script>
