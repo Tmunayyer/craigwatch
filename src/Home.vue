33530ca75d7c142ad22b3933552ef74456740533
tmunayyer <tmunayyer@gmail.com>
2020-07-13 11:03:16 -0400

    card styling for search list

--- src/Home.vue
+++ src/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <SearchForm />
+    <br />
     <SearchList />
   </div>
 </template>
--- src/components/SearchList.vue
+++ src/components/SearchList.vue
@@ -1,8 +1,25 @@
+<style module>
+.search-listitem {
+  box-shadow: 0 0px 2px 0 rgba(0, 0, 0, 0.3);
+  transition: 0.3s;
+  padding: 0.5em;
+  border-radius: 4px;
+}
+
+/* On mouse-over, add a deeper shadow */
+.search-listitem:hover {
+  box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+}
+</style>
+
 <template>
   <ul id="searchList">
     <li v-for="search in searchList" :key="search.ID" v-on:click="redirector(search)">
-      <div>Name: {{ search.Name }}</div>
-      <div>Created On: {{search.CreatedOn}}</div>
+      <div class="search-listitem">
+        <div>Name: {{ search.Name }}</div>
+        <div>Created On: {{search.CreatedOn}}</div>
+      </div>
     </li>
   </ul>
 </template>
