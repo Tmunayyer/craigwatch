dcab2cc19363754f76df8a4b9f34d7eefdee3fc5
tmunayyer <tmunayyer@gmail.com>
2020-07-13 15:17:23 -0400

    restructuring api calls for result list

--- src/Results.vue
+++ src/Results.vue
@@ -9,15 +9,13 @@
   <div class="page-container">
     <ResultSummary v-bind:searchID="searchID" />
     <br />
-    <ResultList v-bind:resultList="resultList" />
+    <ResultList v-bind:searchID="searchID" />
   </div>
 </template>
 
 <script>
 import ResultSummary from "./components/ResultSummary.vue";
 import ResultList from "./components/ResultList.vue";
-
-let _RESULT_FETCH_INTERVAL; // to track the interval for cleanup later
 
 export default {
   name: "Results",
@@ -27,56 +25,8 @@ export default {
   },
   data() {
     return {
-      resultList: [],
-      newListings: [],
-      searchID: this.$route.params.ID,
-      // UnixTimestamp is the cutoff to use when requesting only new listings, should be 0 on load
-      unixDate: 0
+      searchID: this.$route.params.ID
     };
-  },
-  beforeMount: async function() {
-    let initResultList = await this.getResultList();
-
-    if (initResultList.HasNewListings) {
-      this.unixDate = initResultList.Listings[0].UnixDate;
-      this.resultList = initResultList.Listings;
-    } else {
-      setTimeout(async () => {
-        // on new search created, the backend takes a second to get listings.
-        // because of this, retry after 3 seconds and then spawn interval.
-        initResultList = await this.getResultList();
-
-        if (initResultList.HasNewListings) {
-          this.unixDate = initResultList.Listings[0].UnixDate;
-          this.resultList = initResultList.Listings;
-        }
-      }, 3000);
-    }
-
-    // update list every 60 seconds
-    _RESULT_FETCH_INTERVAL = setInterval(async () => {
-      const updatedResultList = await this.getResultList();
-
-      if (updatedResultList.HasNewListings) {
-        this.unixDate = updatedResultList.Listings[0].UnixDate;
-        this.resultList = updatedResultList.Listings.concat(
-          resultList.Listings
-        );
-      }
-    }, 60000);
-  },
-  beforeDestroy: function() {
-    clearInterval(_RESULT_FETCH_INTERVAL);
-  },
-  methods: {
-    getResultList: async function() {
-      const response = await fetch(
-        `/api/v1/listing?ID=${this.searchID}&Datetime=${this.unixDate}`
-      );
-      const list = await response.json();
-
-      return list;
-    }
   }
 };
 </script>
--- src/components/ResultList.vue
+++ src/components/ResultList.vue
@@ -54,10 +54,61 @@
 </template>
 
 <script>
+let _RESULT_FETCH_INTERVAL; // to track the interval for cleanup later
 export default {
   name: "ResultList",
-  props: ["resultList"],
+  props: ["searchID"],
+  data() {
+    return {
+      resultList: [],
+      newListings: [],
+      // UnixTimestamp is the cutoff to use when requesting only new listings, should be 0 on load
+      unixDate: 0
+    };
+  },
+  beforeMount: async function() {
+    let initResultList = await this.getResultList();
+
+    if (initResultList.HasNewListings) {
+      this.unixDate = initResultList.Listings[0].UnixDate;
+      this.resultList = initResultList.Listings;
+    } else {
+      setTimeout(async () => {
+        // on new search created, the backend takes a second to get listings.
+        // because of this, retry after 3 seconds and then spawn interval.
+        initResultList = await this.getResultList();
+
+        if (initResultList.HasNewListings) {
+          this.unixDate = initResultList.Listings[0].UnixDate;
+          this.resultList = initResultList.Listings;
+        }
+      }, 3000);
+    }
+
+    // update list every 60 seconds
+    _RESULT_FETCH_INTERVAL = setInterval(async () => {
+      const updatedResultList = await this.getResultList();
+
+      if (updatedResultList.HasNewListings) {
+        this.unixDate = updatedResultList.Listings[0].UnixDate;
+        this.resultList = updatedResultList.Listings.concat(
+          resultList.Listings
+        );
+      }
+    }, 60000);
+  },
+  beforeDestroy: function() {
+    clearInterval(_RESULT_FETCH_INTERVAL);
+  },
   methods: {
+    getResultList: async function() {
+      const response = await fetch(
+        `/api/v1/listing?ID=${this.searchID}&Datetime=${this.unixDate}`
+      );
+      const list = await response.json();
+
+      return list;
+    },
     formatDate: function(unixTimestamp) {
       var options = {
         year: "numeric",
