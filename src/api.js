21d338b3939cab13c73002802e616c6719946f00
tmunayyer <tmunayyer@gmail.com>
2020-07-15 14:27:32 -0400

    add in a spinner to frontend api function

--- src/api.js
+++ src/api.js
@@ -1,6 +1,11 @@
+import { spinnerState } from "./components/Spinner.vue";
+
 export default async function (url, options) {
+    spinnerState.setLoading(true);
     const response = await fetch(url, options);
     const body = await response.json();
 
+
+    spinnerState.setLoading(false);
     return body;
 }
