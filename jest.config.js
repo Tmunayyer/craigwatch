8957f08cca867b02f6cbd865c943fd65afc0367e
tmunayyer <tmunayyer@gmail.com>
2020-07-15 15:49:13 -0400

    update ignore patterns to transform 3rd party module

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,10 @@ module.exports = {
     },
     transform: {
         "^.+\\.js$": "babel-jest",
-        "^.+\\.vue$": "vue-jest"
+        "^.+\\.vue$": "vue-jest",
     },
     snapshotSerializers: [
         "<rootDir>/node_modules/jest-serializer-vue"
-    ]
+    ],
+    transformIgnorePatterns: ["/node_modules/(?!vue-spinner)"],
 }
