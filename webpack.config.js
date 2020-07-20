7ff992410199c55f00e790ae03ff159656c33447
tmunayyer <tmunayyer@gmail.com>
2020-07-20 15:09:24 -0400

    some custom webpack config to get rid of extra bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,38 @@
+const fs = require('fs');
+
 // webpack.config.js
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// a custom plugin so there arent 1000 bundles when working on frontend
+class TinyTidyPlugin {
+    constructor(options) {
+        this.options = options;
+    }
+
+    apply = (compiler) => {
+        if (compiler.options.mode !== "development") return;
+
+        const callback = this._doSomething.bind(this, compiler);
+        compiler.hooks.beforeCompile.tapAsync('TinyTidyPlugin', callback);
+    };
+
+    _doSomething = function (compiler, details, done) {
+        // remove old bundles according to filename scheme
+        console.log("removing old bundles...");
+        const files = fs.readdirSync(compiler.outputPath);
+        const bundleName = compiler.options.output.filename.split(".");
+        const prefix = bundleName[0];
+
+        for (let i = 0; i < files.length; i++) {
+            if (files[i].startsWith(prefix) && files[i].endsWith('.js')) {
+                fs.unlinkSync(compiler.outputPath + "/" + files[i]);
+            }
+        }
+
+        done();
+    };
+};
 
 module.exports = {
     entry: './src/index.js',
@@ -36,6 +68,7 @@ module.exports = {
         new VueLoaderPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html'
-        })
+        }),
+        new TinyTidyPlugin(),
     ]
 };
