0fa79bf5bdd1938295a9a51e6f9d20c06f5e7284
tmunayyer <tmunayyer@gmail.com>
2020-07-23 15:25:07 -0400

    chart date formatter

--- src/utility.js
+++ src/utility.js
@@ -15,8 +15,28 @@ export default {
             minute: "2-digit"
         };
         const today = new Date(date);
-
         return today.toLocaleDateString("en-US", options);
+    },
 
+    chartDate: function (date) {
+        const translations = {
+            1: "st",
+            2: "nd",
+            3: "rd",
+        };
+        const day = new Date(date).getUTCDate();
+        const time = date.slice(11);
+        let daySuffix = 'th';
+        let remainder = day % 10;
+        if (translations[day] !== undefined) {
+            // 1, 2, 3
+            daySuffix = trnaslations[day];
+        } else if (day > 3 && day < 20) {
+            // 4 - 19
+        } else if (translations[remainder] !== undefined) {
+            // 20+
+            daySuffix = translations[remainder];
+        }
+        return `${day}${daySuffix} ${time}`;
     }
 }
