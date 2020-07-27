41bd479adca472f0d284b46d0b7a7025b7015b80
tmunayyer <tmunayyer@gmail.com>
2020-07-27 09:41:02 -0400

    new unixdate now takes timezone

--- utility.go
+++ utility.go
@@ -14,9 +14,17 @@ func newDate(date string) time.Time {
 	return formattedDate
 }
 
-func newUnixDate(date string) int64 {
+func newUnixDate(date string, tz *time.Location) int64 {
 	layout := "2006-01-02 15:04"
-	formattedDate, err := time.Parse(layout, date[:16])
+	var formattedDate time.Time
+	var err error
+	if tz == nil {
+		loc, _ := time.LoadLocation("America/New_York")
+		formattedDate, err = time.ParseInLocation(layout, date[:16], loc)
+	} else {
+		formattedDate, err = time.ParseInLocation(layout, date[:16], tz)
+	}
+
 	if err != nil {
 		panic(err)
 	}
