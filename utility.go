4e270912045b161348bf13df8b917cea3a7387f5
tmunayyer <tmunayyer@gmail.com>
2020-07-27 09:41:27 -0400

    remove old date function

--- utility.go
+++ utility.go
@@ -4,16 +4,6 @@ import (
 	"time"
 )
 
-func newDate(date string) time.Time {
-	layout := "2006-01-02 15:04"
-	formattedDate, err := time.Parse(layout, date)
-	if err != nil {
-		panic(err)
-	}
-
-	return formattedDate
-}
-
 func newUnixDate(date string, tz *time.Location) int64 {
 	layout := "2006-01-02 15:04"
 	var formattedDate time.Time
